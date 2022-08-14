c783046c76ac173d528be4a3fcd4e51fd999b1ad
linux_china <libing.chen@gmail.com>
2022-08-13 20:48:51 -0700

    build: update to federation-graphql-java-support 2.0.5

--- reviews-subgraph/build.gradle.kts
+++ reviews-subgraph/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	id("org.springframework.boot") version "2.7.2"
-	id("io.spring.dependency-management") version "1.0.12.RELEASE"
+	id("io.spring.dependency-management") version "1.0.13.RELEASE"
 	java
 }
 
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.apollographql.federation:federation-graphql-java-support:2.0.3")
+    implementation("com.apollographql.federation:federation-graphql-java-support:2.0.5")
     implementation("org.springframework.boot:spring-boot-starter-graphql")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
