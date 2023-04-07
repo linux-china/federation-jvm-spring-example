c9e7dd3e4fb2d540919b4cc3d2637a6bbf433901
Libing Chen <libing.chen@gmail.com>
2023-04-07 16:40:18 +0800

    build: update to graphql-java 20.2 and federation-graphql-java-support 3.0.0

--- products-subgraph/build.gradle.kts
+++ products-subgraph/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	id("org.springframework.boot") version "2.7.8"
+	id("org.springframework.boot") version "2.7.10"
 	id("io.spring.dependency-management") version "1.1.0"
 	java
 }
@@ -12,7 +12,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.apollographql.federation:federation-graphql-java-support:2.3.0")
+    implementation("com.graphql-java:graphql-java:20.2")
+    implementation("com.apollographql.federation:federation-graphql-java-support:3.0.0")
     implementation("org.springframework.boot:spring-boot-starter-graphql")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
