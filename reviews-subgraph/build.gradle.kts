01ba85a016c9d07e376326373bdc2b3c24efe0d7
Libing Chen <libing.chen@gmail.com>
2023-05-06 14:58:33 +0800

    build: update dependencies

--- reviews-subgraph/build.gradle.kts
+++ reviews-subgraph/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	id("org.springframework.boot") version "3.0.5"
+	id("org.springframework.boot") version "3.0.6"
 	id("io.spring.dependency-management") version "1.1.0"
 	java
 }
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     implementation("com.graphql-java:graphql-java:20.2")
-    implementation("com.apollographql.federation:federation-graphql-java-support:3.0.0")
+    implementation("com.apollographql.federation:federation-graphql-java-support:3.0.1")
     implementation("org.springframework.boot:spring-boot-starter-graphql")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
